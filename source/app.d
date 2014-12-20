3fdee3740dc1ce89e7fe9773b08175997e83e79c
antoniomasamichi <masa.imoto1984@gmail.com>
2014-12-20 22:59:48 +0900

    second commit

--- source/app.d
+++ source/app.d
@@ -5,51 +5,25 @@ import derelict.sdl2.sdl;
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 640;
 
-SDL_Window gWindow;
+SDL_Window* gWindow = null;
 
-SDL_Surface gHelloWorld;
+SDL_Surface* gScreenSurface = null;
 
 bool init()
 {
     SDL_Init( SDL_INIT_VIDEO );
-    gWindow = 
+    gWindow = SDL_CreateWindow("sample", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    gScreenSurface = SDL_GetWindowSurface( gWindow);
+
+    return true;
+}
 
 
 void main()
 {
     DerelictSDL2.load();
 
-    SDL_Init(SDL_INIT_VIDEO);
-
-    SDL_Window* window = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-
-    screenSurface = SDL_GetWindowSurface(window);
-
-    bool running = true;
-
-    while( running )
-    {
-        SDL_Event e;
-
-        while (SDL_PollEvent(&e))
-        {
-            switch (e.type)
-            {
-                case SDL_KEYDOWN, SDL_QUIT:
-                    running = false;
-                    break;
-
-                default:
-                    break;
-            }
-
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface.format, 0xFF, 0xFF, 0xFF ));
-            SDL_UpdateWindowSurface( window);
-
-
-        }
-    }
-    SDL_DestroyWindow(window);
+    init();
     SDL_Quit();
 
     return;
